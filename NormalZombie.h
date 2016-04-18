56848841fa86dc12fdd0d3f7510d26f927d9e77e
Altez <Altez@users.noreply.github.com>
2016-04-18 22:14:23 +0200

    Added files via upload

---
+++ NormalZombie.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Enemy.h"
+#include "Normal_Zombie_Movment.h"
+
+class NormalZombie :
+	public Enemy
+{
+	Normal_Zombie_Movment normalZombieMovment;
+	Time knockbackTime;
+	Clock knockbackClock;
+	bool isKnockbacked;
+public:
+	void setEnemy(int x, int y, float blockSize, Texture & texture);
+	void move(Vector2f targetPosition);
+	void turnToTarget(Vector2f targetPosition);
+	Sprite getWalkingSprite();
+	void moveWalkingSprite(Vector2f vector);
+	void knockBackOnHit(Vector2f charPosition);
+	NormalZombie();
+	~NormalZombie();
+};
+
